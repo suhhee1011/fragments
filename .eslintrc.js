8619be50ead60c4a5619bca22e8960c9f4eca223
Suhhee Kim <suhhee1011@gmail.com>
2022-01-14 00:22:44 -0500

    Initial work on express app and server

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,9 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
+  extends: 'eslint:recommended',
   parserOptions: {
     ecmaVersion: 13,
   },
-  plugins: ['@typescript-eslint'],
   rules: {},
 };
