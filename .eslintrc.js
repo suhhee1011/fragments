a017eb2b0bf006811ed59f3e725fdf17e5c9650b
Suhhee Kim <suhhee1011@gmail.com>
2022-01-28 22:03:45 -0500

    added Unit test

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,9 @@ module.exports = {
     node: true,
     commonjs: true,
     es2021: true,
+    // Add this next line to configure ESLint for Jest, see:
+    // https://eslint.org/docs/user-guide/configuring/language-options#specifying-environments
+    jest: true,
   },
   extends: 'eslint:recommended',
   parserOptions: {
