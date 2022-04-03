d0ed9d88d4563fa899e9d8459d5eccd8a270f628
Suhhee Kim <suhhee1011@gmail.com>
2022-04-02 23:49:26 -0400

    changes for deploy

--- src/model/data/aws/index.js
+++ src/model/data/aws/index.js
@@ -1,4 +1,4 @@
-const MemoryDB = require('./memory-db');
+const MemoryDB = require('../memory/memory-db');
 const s3Client = require('./s3Client');
 const logger = require('../../../logger');
 const { PutObjectCommand, GetObjectCommand, DeleteObjectCommand } = require('@aws-sdk/client-s3');
